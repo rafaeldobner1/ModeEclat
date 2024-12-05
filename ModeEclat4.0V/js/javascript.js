44d57675736cf2fcf3252bc33f396b1fb3c17c65
rafaeldobner1 <rafaelvdobner@gmail.com>
2024-12-04 22:47:59 -0300

    Delete ModeEclat4.0V directory

--- ModeEclat4.0V/js/javascript.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("shop-now").addEventListener("click", function() {
-    alert("Redirecionando para a seção de produtos!");
-    window.location.href = "#produtos";
-});
-
-function toggleFavorite(element) {
-    element.classList.toggle('favorited');
-}
-
